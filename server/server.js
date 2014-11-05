54f784bc8682ef0db30119d70bdb917018557edc
sebastian jonasson <sebastianjonasson@gmail.com>
2014-11-05 18:14:20 +0100

    Added rest endpoint to server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+app.get('/', function(req, res) {
+	res.send("hello world");
+})
+
 io.on('connection', function(){
 	 /* … */ 
 });
