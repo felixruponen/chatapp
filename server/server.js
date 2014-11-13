2ac1c1003f863b9cece2b30990cd0bbfb24a4c6b
sebastian jonasson <sebastianjonasson@gmail.com>
2014-11-13 18:27:53 +0100

    Disconnection management, removed disconnected user from and list and printing disconnect-message

--- server/server.js
+++ server/server.js
@@ -8,11 +8,13 @@ var express = require('express'),
 app.use('/client', express.static('../front'));
 
 io.on('connection', function(socket){
+
 	console.log("user connected");
 	socket.on('username', function(name){
 		socket.username = name;
 		sockets.push(socket);
 		io.emit('newUser', name);	
+		console.log(socket.id);
 	});
 
 	socket.on('getUsers', function() {
@@ -29,7 +31,22 @@ io.on('connection', function(socket){
 		}
     	io.emit('message', messageData);
 	});
+
+	socket.on('disconnect', function() {
+		
+		console.log("user disconnected");
+		var index = _.indexOf(sockets, socket);
+		sockets.splice(index, 1);
+		console.log(index);
+		_.each(sockets, function(s) {
+			console.log(s.username);
+		})
+		io.emit('allUsers', _.pluck(sockets, 'username'));
+	    io.emit('message','user disconnected');
+	});
 });
+
+
 
 
 
